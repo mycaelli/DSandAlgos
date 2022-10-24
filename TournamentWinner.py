172a51871ca31add6ba35c4e94b062a0a0f41f72
mycaelli <mycaelli@usp.br>
2022-10-24 12:40:47 -0300

    TournamentWinner code

---
+++ TournamentWinner.py
@@ -0,0 +1,25 @@
+'''
+input: 2 arrays, competitions and results
+      competitions has the form [homeTeam, awayTeam], each team is a String 
+      results has information about the winner of each competition, 
+        results[i] denotes the winner of competitions[i]. 
+        In results array, 1 means the homeTeam won and 0 means the awayTeam won.
+
+output: The winner of the tournament
+'''
+def tournamentWinner(competitions, results):
+    winner = {}
+
+    for idx, i in enumerate(results):
+      if i == 0:
+        try:
+          winner[competitions[idx][1]] += 1
+        except:
+          winner[competitions[idx][1]] = 1
+      else:
+        try:
+          winner[competitions[idx][0]] += 1
+        except:
+          winner[competitions[idx][0]] = 1
+        
+    return max(winner, key=winner.get)
